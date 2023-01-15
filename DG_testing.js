5a4eb9d790eaeccae99f6bcad74d218b0957591c
illia-sliusar <84412367+illia-sliusar@users.noreply.github.com>
2023-01-15 22:30:21 +0100

    fill team name and start filling select date

--- DG_testing.js
+++ DG_testing.js
@@ -12,7 +12,7 @@ intent('create event', p => {
     p.play({command: 'createEvent'});
 });
 
-intent(`(yes|okey|confirm)`, p => {
+intent(`(yes|okey|confirm|Correct|Sure|Absolutely|Naturally|Definitely)`, p => {
     if (p.visual) {
         console.log(p.visual)
         if (p.visual.isOpenActiveEvent) {
@@ -23,13 +23,23 @@ intent(`(yes|okey|confirm)`, p => {
         }
     }
 });
-intent(`(open|edit|fill|) $(ORDINAL) (field|)`, p => {
-    let number = p.NUMBER ? p.NUMBER.number : 1;
-    console.log(p)
-    console.log(number)
+intent(`(open|edit|fill|) $(D first|second|third) (field|)`, p => {
+    let string = p.D ? p.D.value : "first";
+    let number = string === "first" ? 1 : string === "second" ? 2 : 3
     if (p.visual) {
         if (p.visual.routeName === "manageEvent") {
-            p.play({command: 'editFiledByNumber', item: number}); 
+            switch (number) {
+                case 1:
+                    p.play({command: "editStartDate"});
+                    break;
+                case 2: 
+                    p.play('What is your Team Name?');
+                    handleTeamName(p)
+                    break;
+                case 3:
+                    p.play('What is your Team Activities?');
+                    break;
+            } 
         }
     }
 });
@@ -39,9 +49,9 @@ const handleTeamName = async (p) => {
     p.play(`is your team name: ${value}`);
     let userDecision = await p.then(handleUnswer);
     if (userDecision) {
-        
+
     } else {
-        p.play('(OK, no problem lets try again)');
+        p.play('(Okey, no problem lets try again)');
         handleTeamName(p)
     }
 }
@@ -50,6 +60,20 @@ intent(`(open|edit|fill|) team name`, async p => {
         if (p.visual.routeName === "manageEvent") {
             p.play('What is your Team Name?');
             handleTeamName(p)
+        }
+    }
+});
+intent(`(open|edit|fill|) select date`, async p => {
+    if (p.visual) {
+        if (p.visual.routeName === "manageEvent") {
+            p.play({command: "editStartDate"});
+        }
+    }
+});
+intent(`(open|edit|fill|) 4 day Fundraising window`, async p => {
+    if (p.visual) {
+        if (p.visual.routeName === "manageEvent") {
+            p.play({command: "editStartDate"});
         }
     }
 });
@@ -91,13 +115,31 @@ intent(`open active event`, p => {
 
 // Do you want create event?
 // select Date > min/max date + blackout dates
-// team name 
 // select activity > states + new store
 // zip > validation
 
+const handleSelectDate = async (p) => {
+    const [value, moment] = await p.then(userInputDate);
+    p.play({command: "readStartDate", value: moment.toDate()});
+    p.play(`is selected Date correct: ${value}`);
+    let userDecision = await p.then(handleUnswer);
+    if (userDecision) {
+
+    } else {
+        p.play('(Okey, no problem lets try again)');
+        handleSelectDate(p)
+    }
+}
+const userInputDate = context(() => {
+    intent("$(Date|DateTime)", p => p.resolve(p.DATE));
+})
+
 projectAPI.runText = function(p, param, callback) {
-    console.log(param);
     p.play(`${param.text}`);
+};
+projectAPI.runSelectDay = function(p, param, callback) {
+    p.play(`${param.text}`);
+    handleSelectDate(p)
 };
 
 projectAPI.onboardingTakeover = function(p, param, callback) {
@@ -109,9 +151,9 @@ projectAPI.onboardingTakeover = function(p, param, callback) {
 };
 projectAPI.createEvent = function(p, param, callback) {
     p.play(`Let's Schedule a fundraising event.
-  Please fill next fileds
-  first. select day. 4-day Fundraising window
-  second. Team Name
-  third. Team Activity
+  Please fill the next fileds. 
+  first. select day. 4-day Fundraising window.
+  second. Team Name.
+  third. Team Activity.
 To fill the field say the number of the field or field name.`);
 };
